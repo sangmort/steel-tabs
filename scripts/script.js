043bc69a2bfa191e138736ec62ab796abee0e0d1
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-28 14:12:50 -0500

    Add some tomlinisms

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let tomlinism = [
+    "Black and Gold doesn’t fold.",
+    "The standard is the standard.",
+    "We're still squirreling those nuts.",
+    "There’s a fine line between drinking wine and squashing grapes.",
+    "We need volunteers, not hostages.",
+    "Can’t do ordinary things and expect unique results."
+  ];
+  
