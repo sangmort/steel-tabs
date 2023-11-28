372a88d6ee2b2b11ffeaf90303cff473af5ccb48
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-28 16:39:23 -0500

    Detect if qoute is too long, add .shrink-text

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ let tomlinism = [
   "There’s a fine line between drinking wine and squashing grapes.",
   "We need volunteers, not hostages.",
   "Can’t do ordinary things and expect unique results.",
+  "It’s not what you’re capable of, but it’s about what you’re willing to do. We put our expectations based on our capabilities. <br>And, really, we should put our expectations based on performance, based on the work we put in. <br>It applies to football, but it applies to every component of life. ",
 ];
 
 // Randomizer
@@ -14,8 +15,16 @@ function selectRandomItemFrom(array) {
 
 // Selects toliminism, applies randomizer, injects into the page
 function tomlinismGenerator() {
-  let randomTomlinQuote = selectRandomItemFrom(tomlinism);
-  document.getElementById("tomlinism-quote").innerHTML = '"' + randomTomlinQuote + '"';
-}
+    let randomTomlinQuote = selectRandomItemFrom(tomlinism);
+    let quoteElement = document.getElementById("tomlinism-quote");
+    quoteElement.innerHTML = '"' + randomTomlinQuote + '"';
+
+    // Check if the quote is long and add a CSS class to shrink text size
+    if (randomTomlinQuote.length > 100) {
+      quoteElement.classList.add("shrink-text");
+    } else {
+      quoteElement.classList.remove("shrink-text");
+    }
+  }
 
 tomlinismGenerator();
