a194508ea46cf5a13240d5422f2a3de92a61ed83
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-28 14:15:04 -0500

    Add random generator helper function

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 let tomlinism = [
-    "Black and Gold doesn’t fold.",
-    "The standard is the standard.",
-    "We're still squirreling those nuts.",
-    "There’s a fine line between drinking wine and squashing grapes.",
-    "We need volunteers, not hostages.",
-    "Can’t do ordinary things and expect unique results."
-  ];
-  
+  "Black and Gold doesn’t fold.",
+  "The standard is the standard.",
+  "We're still squirreling those nuts.",
+  "There’s a fine line between drinking wine and squashing grapes.",
+  "We need volunteers, not hostages.",
+  "Can’t do ordinary things and expect unique results.",
+];
+
+// Random generator
+function selectRandomItemFrom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
